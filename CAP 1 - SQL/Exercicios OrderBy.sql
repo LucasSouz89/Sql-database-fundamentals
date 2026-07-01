a8d9db53cf9bb226cde500d1bcc1b45f75fa58c4
Lucas Souza <lucas.ss7@outlook.com>
2026-07-01 11:30:55 -0300

    Aprendendo sobre Order by

---
+++ CAP 1 - SQL/Exercicios OrderBy.sql
@@ -0,0 +1,12 @@
+#EXERCICIO 1
+SELECT city_id,city
+FROM city
+ORDER BY city ASC;
+#EXERCICIO 2
+SELECT title,replacement_cost 
+FROM film
+ORDER BY replacement_cost DESC;
+#EXERCICIO 3
+Select payment_id,amount
+FROM payment
+ORDER BY amount;
