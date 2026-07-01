cc7655bc72dde8cdb33e97899c1141962ecc8435
Lucas Souza <lucas.ss7@outlook.com>
2026-07-01 11:02:20 -0300

    docs: setup inicial do repositorio e primeiros exercicios de DQL

---
+++ CAP 1 - SQL/Execicios SELECT,FROM.sql
@@ -0,0 +1,6 @@
+#Exercico 1
+SELECT first_name,last_name,email,active FROM customer;
+#Exercicio 2
+SELECT title,description,release_year FROM film;
+#Exercicio 3
+SELECT inventory_id,film_id FROM inventory;
