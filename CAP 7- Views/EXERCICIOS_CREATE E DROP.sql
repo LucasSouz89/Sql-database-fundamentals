cda7d6d599004e9999125cbdbd6f2246f04d3bfe
Lucas Souza <lucas.ss7@outlook.com>
2026-07-18 11:02:26 -0300

    fazendo exercicios do cap 7

--- CAP 7- Views/EXERCICIOS_CREATE E DROP.sql
+++ CAP 7- Views/EXERCICIOS_CREATE E DROP.sql
@@ -20,7 +20,7 @@ HAVING contagem > 30
 ORDER BY  contagem DESC;
 #4
 CREATE VIEW resumo_categorias AS
-SELECT c.name,AVG(f.rental_rate) AS tempo_medio
+SELECT c.name,AVG(f.rental_rate) AS valor_medio
 FROM category c
 JOIN film_category fc USING(category_id)
 JOIN film f USING(film_id)
@@ -31,3 +31,32 @@ SELECT s.first_name,s.last_name,SUM(p.amount) AS pagamentos
 FROM staff s
 JOIN payment p USING(staff_id)
 GROUP BY s.staff_id,s.first_name,s.last_name;
+#EXERCICIOS DROP,REPLACE
+#1
+CREATE OR REPLACE VIEW resumo_categorias AS
+SELECT c.name,AVG(f.rental_duration) AS tempo_medio
+FROM category c
+JOIN film_category fc USING(category_id)
+JOIN film f USING(film_id)
+GROUP BY c.category_id
+ORDER BY tempo_medio DESC;
+#2
+DROP VIEW estoque_por_loja;
+#3
+CREATE OR REPLACE VIEW ator_filmografia AS
+SELECT a.actor_id AS ID,a.first_name,a.last_name, COUNT(fa.actor_id)
+FROM actor a
+JOIN film_actor fa USING(actor_id)
+GROUP BY a.actor_id,a.first_name,a.last_name;
+
+#4
+DROP VIEW IF EXISTS temp_relatorio;
+
+#5
+CREATE OR REPLACE VIEW clientes_top_alugueis AS
+SELECT c.first_name,c.last_name,c.email, COUNT(p.amount) AS contagem
+FROM customer c
+JOIN payment p USING(customer_id)
+GROUP BY c.customer_id
+HAVING contagem > 30
+ORDER BY  contagem DESC;
